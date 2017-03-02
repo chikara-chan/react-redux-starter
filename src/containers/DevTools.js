02f278862705a6c6db8a37ffb60d7b475d889aca
chikara <chenhongtu@51xianqu.net>
2017-03-02 10:49:20 +0800

    build: change redux data monitor

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import {createDevTools} from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import ChartMonitor from 'redux-devtools-chart-monitor'
 
 export default createDevTools(
-  <DockMonitor defaultPosition="right"
+  <DockMonitor defaultPosition="bottom"
     defaultIsVisible={false}
     toggleVisibilityKey="ctrl-h"
     changePositionKey="ctrl-w">
-    <LogMonitor/>
+    <ChartMonitor style={{
+      padding: '0 20px',
+      background: '#fff',
+      text: {
+        colors: {
+          hover: '#378cbe'
+        }
+      }
+    }}
+      tooltipOptions={{
+        style: {
+          background: '#fff',
+          opacity: '0.9',
+          borderRadius: '5px',
+          border: '1px solid #dbdbdb',
+          padding: '5px'
+        }
+      }}/>
   </DockMonitor>
 )
